c2c3eaf91e3b67aef36e2a3fd10c2be39e728291
Dave Cross <dave@dave.org.uk>
2022-09-17 16:39:22 +0100

    Initial version

---
+++ js/links.js
@@ -0,0 +1,63 @@
+$(document).ready( function() {
+  $.getJSON( "/links.json", function ( data ) {
+    $('#img').children('img').attr('src', '/img/' + data.image);
+    $('#name h1').text(data.name + ' / @' + data.handle);
+    $('title').text(data.name + ' / @' + data.handle);
+    $('#desc').text(data.desc);
+
+    $.each(data.social, function( index, value ) {
+      console.log(value);
+      let link = mk_social_link(value, data.handle);
+      $('#social p').append(
+        $('<a>').attr('href', link).append(
+          $('<i>').attr('class', 'fa-brands fa-' + value.service + ' fa-3x')
+        )
+      );
+    });
+    console.log($('#social p').html());
+
+    $.each(data.links, function( index, value ) {
+      $('#links ul').append(
+        $('<li>').append(
+          $('<a>').attr('href', value.link).append(value.title)
+        )
+      );
+    });
+  })
+  .fail(function(jqXHR, stat, err) {
+    console.log( stat + ': ' + err );
+  })
+});
+
+function mk_social_link (social, default_handle) {
+  let urls = {
+    "facebook": "https://facebook.com/",
+    "twitter": "https://twitter.com/",
+    "instagram": "https://instagram.com/",
+    "tiktok": "https://tiktok.com/@",
+    "linkedin": "https://linkedin.com/in/",
+    "email": "mailto:",
+    "substack": "https://XXXX.substack.com/",
+    "github": "https://github.com/",
+    "medium": "https://XXXX.medium.com/"
+  };
+
+  let handle = social.handle || default_handle;
+
+  let url;
+
+  if (social.service in urls) {
+    url = urls[social.service];
+  } else {
+    console.log('Unknown social service: ' + social.service);
+    return "";
+  }
+
+  if (url.match('XXXX')) {
+    url = url.replace('XXXX', handle);
+  } else {
+    url = url + handle;
+  }
+
+  return(url);
+}
