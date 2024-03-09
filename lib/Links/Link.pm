d987eec3b962dd0b1e58af960f099ac2d537be21
Dave Cross <dave@dave.org.uk>
2024-03-09 13:00:30 +0000

    Add subtitles to some links

--- lib/Links/Link.pm
+++ lib/Links/Link.pm
@@ -2,13 +2,17 @@ use Feature::Compat::Class;
 
 class Links::Link {
   field $title :param;
+  field $subtitle :param = '';
   field $link :param;
 
   method title { return $title }
+  method subtitle { return $subtitle }
   method link { return $link }
 
   method mk_link {
-    return q[<li class='list-group-item list-group-item-action'><a href="] .
-      $self->link . q[">] . $self->title . q[</a></li>];
+    my $a_tag = q[<a href="] . $self->link . q[">] . $self->title . q[</a>];
+    my $subtitle = $self->subtitle ? q[<br><small class="text-muted">] . $self->subtitle . q[</small>] : '';
+
+    return q[<li class='list-group-item list-group-item-action'>] . $a_tag . $subtitle . q[</li>];
   }
 }
