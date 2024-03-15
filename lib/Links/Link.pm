0ab00991c5a3a95354d1d5e39a618638998b99d6
Dave Cross <dave@dave.org.uk>
2024-03-15 10:30:08 +0000

    Switch to Bootstrap 5.x.x

--- lib/Links/Link.pm
+++ lib/Links/Link.pm
@@ -10,7 +10,7 @@ class Links::Link {
   method link { return $link }
 
   method mk_link {
-    my $a_tag = q[<a href="] . $self->link . q[">] . $self->title . q[</a>];
+    my $a_tag = q[<a href="] . $self->link . q[" class="text-decoration-none">] . $self->title . q[</a>];
     my $subtitle = $self->subtitle ? q[<br><small class="text-muted">] . $self->subtitle . q[</small>] : '';
 
     return q[<li class='list-group-item list-group-item-action'>] . $a_tag . $subtitle . q[</li>];
